f44dd665fc797bbcfed2e757d128a96fd2049c3d
anthony <ac.nicholls@gmail.com>
2025-09-01 17:30:12 -0400

    modify to read/write current IP to file.

--- Public IP Address Check/Start-PublicIPAddressCheck.ps1
+++ Public IP Address Check/Start-PublicIPAddressCheck.ps1
@@ -2,33 +2,34 @@
 # Script Name: Public IP Address Check
 # Script Purpose: Sends and email alert if the public IP address of the device changes
 # Created By: Woodward.Digital
+# Edited By: acnicholls
 # Website: https://woodward.digital
 # Email: contact@woodward.digital
-# Version: v1.0
+# Version: v1.1
 ###########################################################################################
 
 # Variables
 ###########################################################################################
 
-# Find currenlt public IP Address
+# Find current public IP Address
 # Run the following command in a PowerShell Console
 # (Invoke-WebRequest ipecho.net/plain).content
 
 # Enter the current public IP address below
-$CurrentPublicIP = "8.8.8.8"
+$CurrentPublicIP = Get-Content -Path ".\currentip.txt"
 
 # Email alert settings
 
 # Where to send the email alerts to
-$EmailTo = "<Email Address>"
+$EmailTo = "acnicholls@gmail.com"
 
 # SMTP Server details
-$SMTPServer = "<SMTP Server Address>"
-$SMTPPort = "<SMTP Server Port>"
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = 587
 
 # SMTP authentication details
-$EmailAuthUser = "<SMTP Username>"
-$EmailAuthPassword = "<SMTP Password>"
+$EmailAuthUser = "<needs a gmail account>"
+$EmailAuthPassword = "<needs an app password>"
 
 ###########################################################################################
 
@@ -38,12 +39,13 @@ $EmailAuth = New-Object System.Management.Automation.PSCredential ($EmailAuthUse
 
 
 # Check public IP address
-if ((Invoke-WebRequest ipecho.net/plain).content -eq $CurrentPublicIP){
-# IP address hasn't changed
-# Uncomment line below if you want to recive an email to confrim it hasnt changed
-# Send-MailMessage -Body "IP Address is has not changed" -From $EmailAuthUser -To $EmailTo -Subject "IP address has not changed" -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $EmailAuth
+if ((Invoke-WebRequest ipecho.net/plain).content -eq $CurrentPublicIP) {
+    # IP address hasn't changed
+    # Uncomment line below if you want to recive an email to confrim it hasnt changed
+    Send-MailMessage -Body "IP Address is has not changed" -From $EmailAuthUser -To $EmailTo -Subject "IP address has not changed" -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $EmailAuth
 }
-Else{
-# IP address has changed
-Send-MailMessage -Body "IP Address is has changed" -From $EmailAuthUser -To $EmailTo -Subject "IP Address Change - ALERT" -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $EmailAuth
+Else {
+    # IP address has changed
+    Send-MailMessage -Body "IP Address is has changed" -From $EmailAuthUser -To $EmailTo -Subject "IP Address Change - ALERT" -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $EmailAuth
+    (Invoke-WebRequest ipecho.net/plain).content > ".\currentip.txt"
 }
